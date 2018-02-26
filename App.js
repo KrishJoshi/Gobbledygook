2f25230678c6c4ac5cf8fac10c6973ff55007988
Krish <krijoshi@cisco.com>
2018-02-26 11:34:35 +0000

    detached from crna

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {Container, Heading, ContainerBackgroundStyles, Quote, RoundedBox} from '
 import {Font} from 'expo';
 import {AnimatableCat} from './components/cat';
 import background from './assets/background.png';
-import {ImageBackground, StatusBar} from 'react-native';
+import {ImageBackground, StatusBar, TouchableHighlight} from 'react-native';
+import RNShakeEvent from 'react-native-shake-event';
 
 export default class App extends React.Component {
   constructor() {
@@ -13,8 +14,15 @@ export default class App extends React.Component {
   componentWillMount() {
     StatusBar.setBarStyle('light-content', true);
     this.setState({
-      fontLoaded: false
-    })
+      fontLoaded: false,
+      shakeText: 'Shake your phone'
+    });
+
+    RNShakeEvent.addEventListener('shake', () => {
+      this.setState({
+        shakeText: 'Done!'
+      })
+    });
   }
 
   async componentDidMount() {
@@ -24,15 +32,23 @@ export default class App extends React.Component {
     this.setState({fontLoaded: true});
   }
 
+  _onClick = () =>
+    this.setState({
+      shakeText: 'Done!'
+    });
+
+
   render() {
     return this.state.fontLoaded ? (
       <ImageBackground source={background} style={ContainerBackgroundStyles}>
         <Container>
           <Heading>Gobbledygook</Heading>
           <AnimatableCat animation="slideInDown" iterationCount={5} direction="alternate"/>
-          <RoundedBox>
-            <Quote>Shake your phone</Quote>
-          </RoundedBox>
+          <TouchableHighlight onPress={this._onClick}>
+            <RoundedBox>
+              <Quote onClick={this._onClick}>{this.state.shakeText}</Quote>
+            </RoundedBox>
+          </TouchableHighlight>
         </Container>
       </ImageBackground>
     ) : null;
